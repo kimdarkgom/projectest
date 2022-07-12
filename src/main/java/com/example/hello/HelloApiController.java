2e4cce838d6c7a7161051225a1fe9670bf071544
hsj <seungjung.hwang@gmail.com>
2022-07-12 15:42:05 +0900

    스프링 부트 JPA 마리아DB 데이터 보기 추가

--- src/main/java/com/example/hello/HelloApiController.java
+++ src/main/java/com/example/hello/HelloApiController.java
@@ -5,9 +5,12 @@ import com.example.hello.vo.MenuVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloApiController {
@@ -26,6 +29,12 @@ public class HelloApiController {
     @PostMapping("/insert")
     public ResponseEntity<MenuVo> save(MenuVo menu) {
         return new ResponseEntity<MenuVo>(menuService.save(menu), HttpStatus.OK);
+    }
+
+    @GetMapping("/menulist")
+    public ResponseEntity<List<MenuVo>> getAllMenu() {
+        List<MenuVo> list = menuService.findAll();
+        return new ResponseEntity<List<MenuVo>>(list, HttpStatus.OK);
     }
 
 }
